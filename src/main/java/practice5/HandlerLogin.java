1a47baeddadb5690cbc6c78a837ef81f4c3b170b
OdarkaDetroit <dasha.shmyelova@outlook.com>
2021-06-21 15:31:00 +0300

    added handler for goods and login

---
+++ src/main/java/practice5/HandlerLogin.java
@@ -0,0 +1,60 @@
+package practice5;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.codec.digest.DigestUtils;
+import practice4.ProductDB;
+
+import java.io.IOException;
+
+public class HandlerLogin implements HttpHandler {
+
+    private static HttpServer server;
+    private static ProductDB db;
+
+    HandlerLogin(ProductDB db){
+        super();
+        this.db = db;
+    }
+
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        if (httpExchange.getRequestMethod().equals("POST")) {
+            ObjectMapper om = new ObjectMapper();
+            User user = om.readValue(httpExchange.getRequestBody(), User.class);
+
+            User fromDB = db.getByLogin(user.getLogin());
+
+            if (fromDB != null) {
+
+                if (fromDB.getPassword().equals(DigestUtils.md5Hex(user.getPassword()))) {
+                    Tokens t = new Tokens();
+                    String token = t.createJWT(fromDB.getLogin());
+                    httpExchange.getResponseHeaders()
+                            .set("Authorization", token);
+
+                    sendResponse(httpExchange, 200,"token - " + token + "\n" + "ok" );
+                } else {
+                    sendResponse(httpExchange, 401,"some problems with password");
+                }
+
+            } else {
+                sendResponse(httpExchange, 401,"some problems with user");
+            }
+
+        } else {
+            httpExchange.sendResponseHeaders(405, 0);
+        }
+        httpExchange.close();
+    }
+
+    private static void sendResponse(HttpExchange httpExchange, int number, String body) throws IOException {
+        byte[] response = body.getBytes();
+        httpExchange.sendResponseHeaders(number, response.length);
+        httpExchange.getResponseBody().write(response);
+        httpExchange.getResponseBody().flush();
+    }
+}
